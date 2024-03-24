01eb7642d4b71a9b7eabdf9f1b959a394f2e1b0a
Max Groiser <maximiliangroiser@gmail.com>
2024-03-24 01:14:12 +0100

    Implemented app disablement while editing connection

---
+++ ViscaCamLink/Util/BoolAndConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace ViscaCamLink.Util;
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+public class BoolAndConverter : IMultiValueConverter
+{
+    public Object Convert(Object[] values, Type targetType, Object parameter, CultureInfo culture)
+    {
+        return values.Cast<Boolean>().All(value => value);
+    }
+
+    public Object[] ConvertBack(Object value, Type[] targetTypes, Object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
