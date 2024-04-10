c21c6b8cd9716e844bc498350c755c095d63763b
Max Groiser <maximiliangroiser@gmail.com>
2024-04-10 22:46:38 +0200

    Added language setting

---
+++ ViscaCamLink/Util/Language.cs
@@ -0,0 +1,34 @@
+﻿namespace ViscaCamLink.Util;
+
+using System;
+using System.ComponentModel;
+
+using ViscaCamLink.Resources;
+
+[Serializable]
+public enum Language
+{
+    System = 0,
+   [Description("en-US")]
+    English = 1,
+   [Description("de-DE")]
+    German = 2,
+}
+
+public static class LanguageExtension
+{
+    public static String ToLocalizedString(this Language language)
+    {
+        switch (language) 
+        {
+            case Language.System:
+                return Strings.Language_System;
+            case Language.English:
+                return Strings.Language_English;
+            case Language.German:
+                return Strings.Language_German;
+            default:
+                return language.ToString();
+        }
+    }
+}
