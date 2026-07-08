d70b7780e41750476bdc15e892d74e2c4270d363
Max Groiser <maximiliangroiser@gmail.com>
2026-07-08 08:30:39 +0200

    Make main windows handling more robust

--- ViscaCamLink/Views/ViscaCamLinkView.xaml.cs
+++ ViscaCamLink/Views/ViscaCamLinkView.xaml.cs
@@ -2,17 +2,28 @@ namespace ViscaCamLink.Views;
 
 using System;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
+
 using ViscaCamLink.Infrastructure.Interface;
 using ViscaCamLink.Services;
 using ViscaCamLink.ViewModels;
+
 using WpfAnimatedGif;
 
 public partial class ViscaCamLinkView : Window
 {
+    [LibraryImport("user32.dll", EntryPoint = "GetWindowLongA")]
+    private static partial int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [LibraryImport("user32.dll", EntryPoint = "SetWindowLongA")]
+    private static partial int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
     private readonly ISettingsService _settingsService;
+
     private WindowState _restoredWindowState = WindowState.Normal;
 
     public ViscaCamLinkView(ISettingsService settingsService)
@@ -22,10 +33,13 @@ public partial class ViscaCamLinkView : Window
         // Pre-set Width before InitializeComponent so the HWND is created at the correct
         // size. SetWindowPlacement in SourceInitialized then only moves the window (no
         // resize) which avoids the DWM black flash on startup.
-        if (_settingsService.WindowPlacement is { } p)
-            Width = p.NormalRight - p.NormalLeft;
+        if (_settingsService.WindowPlacement is { } placement)
+        {
+            Width = placement.NormalRight - placement.NormalLeft;
+        }
 
         InitializeComponent();
+
         SourceInitialized += OnSourceInitialized;
         ContentRendered += OnContentRendered;
         Closing += OnClosing;
@@ -33,8 +47,21 @@ public partial class ViscaCamLinkView : Window
 
     private void OnSourceInitialized(object? sender, EventArgs e)
     {
+        const int GWL_STYLE = -16;
+        const int WS_MAXIMIZEBOX = 0x10000;
+
+        if (sender is Window window)
+        {
+            var hwnd = new WindowInteropHelper(window).Handle;
+            var value = GetWindowLong(hwnd, GWL_STYLE);
+
+            _ = SetWindowLong(hwnd, GWL_STYLE, (int)(value & ~WS_MAXIMIZEBOX));
+        }
+
         if (_settingsService.WindowPlacement is { } placement)
+        {
             _restoredWindowState = WindowPlacementHelper.Restore(this, placement);
+        }
     }
 
     private void OnContentRendered(object? sender, EventArgs e)
